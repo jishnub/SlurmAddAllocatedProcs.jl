0feb3635467fa215bd309957b70291c7c8fd2baa
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2021-10-28 11:41:04 +0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.22

---
+++ src/EasyClusterManagersSlurm.jl
@@ -0,0 +1,5 @@
+module EasyClusterManagersSlurm
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using EasyClusterManagersSlurm
+using Test
+
+@testset "EasyClusterManagersSlurm.jl" begin
+    # Write your tests here.
+end
